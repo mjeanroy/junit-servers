393249671362126f6e16cf02ba9388f8e4dccf32
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2022-12-18 14:35:27 +0100

    feat: add jetty-10 sample

---
+++ junit-servers-samples/junit-servers-samples-jupiter/junit-servers-samples-jupiter-spring-jetty-10/src/test/java/com/github/mjeanroy/junit/servers/samples/jetty/jupiter/IndexWithSpecializedExtensionTest.java
@@ -0,0 +1,48 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2022 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.junit.servers.samples.jetty.jupiter;
+
+import com.github.mjeanroy.junit.servers.annotations.TestServerConfiguration;
+import com.github.mjeanroy.junit.servers.client.HttpClient;
+import com.github.mjeanroy.junit.servers.jetty10.EmbeddedJetty;
+import com.github.mjeanroy.junit.servers.jetty10.EmbeddedJettyConfiguration;
+import com.github.mjeanroy.junit.servers.jetty10.jupiter.JettyServerExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static com.github.mjeanroy.junit.servers.samples.jetty.jupiter.utils.EmbeddedJetty10ConfigurationFactory.createJetty10Configuration;
+import static com.github.mjeanroy.junit.servers.samples.utils.EmbeddedWebAppTestUtils.ensureWebAppIsOk;
+
+@ExtendWith(JettyServerExtension.class)
+class IndexWithSpecializedExtensionTest {
+
+	@TestServerConfiguration
+	private static EmbeddedJettyConfiguration configuration = createJetty10Configuration();
+
+	@Test
+	void it_should_have_an_index(HttpClient client, EmbeddedJetty jetty) {
+		ensureWebAppIsOk(client, jetty);
+	}
+}
---
+++ junit-servers-samples/junit-servers-samples-jupiter/junit-servers-samples-jupiter-spring-jetty-10/src/test/java/com/github/mjeanroy/junit/servers/samples/jetty/jupiter/utils/EmbeddedJetty10ConfigurationFactory.java
@@ -0,0 +1,54 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2022 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.junit.servers.samples.jetty.jupiter.utils;
+
+import com.github.mjeanroy.junit.servers.jetty10.EmbeddedJettyConfiguration;
+
+import java.io.File;
+import java.net.URL;
+
+public final class EmbeddedJetty10ConfigurationFactory {
+
+	public static EmbeddedJettyConfiguration createJetty10Configuration() {
+		try {
+			EmbeddedJettyConfiguration.Builder builder = EmbeddedJettyConfiguration.builder()
+					.withWebapp("src/main/webapp")
+					.withClasspath("target/classes")
+					.withContainerJarPattern(".*\\.jar");
+
+			// Note use of maven plugin to copy a maven dependency to this directory
+			File file = new File("target/lib/");
+			if (file.exists()) {
+				URL parentClasspath = file.toURI().toURL();
+				builder.withParentClasspath(parentClasspath);
+			}
+
+			return builder.build();
+		}
+		catch (Exception ex) {
+			throw new AssertionError(ex);
+		}
+	}
+}
