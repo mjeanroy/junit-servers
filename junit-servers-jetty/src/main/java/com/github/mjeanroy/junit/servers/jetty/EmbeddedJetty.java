43f05f6c4ace3f3394c9183a5618a58f67c3a154
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2022-12-24 17:59:05 +0100

    feat: add deprecated junit-servers-jetty module

---
+++ junit-servers-jetty/src/main/java/com/github/mjeanroy/junit/servers/jetty/EmbeddedJetty.java
@@ -0,0 +1,79 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2022 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.junit.servers.jetty;
+
+import com.github.mjeanroy.junit.servers.loggers.Logger;
+import com.github.mjeanroy.junit.servers.loggers.LoggerFactory;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+
+import javax.servlet.ServletContext;
+
+import static com.github.mjeanroy.junit.servers.jetty.EmbeddedJettyConfiguration.defaultConfiguration;
+
+/**
+ * Jetty Embedded Server.
+ *
+ * @deprecated Use {@code junit-servers-jetty-9} instead.
+ */
+@Deprecated
+public class EmbeddedJetty extends AbstractEmbeddedJetty<EmbeddedJettyConfiguration> {
+
+	private static final Logger log = LoggerFactory.getLogger(EmbeddedJetty.class);
+
+	/**
+	 * Build default embedded jetty server.
+	 */
+	public EmbeddedJetty() {
+		this(defaultConfiguration());
+		log.warn("{} is deprecated and will be removed in the next major release, use junit-servers-tomcat-8 instead, see https://mjeanroy.dev/junit-servers", EmbeddedJetty.class);
+	}
+
+	/**
+	 * Build embedded jetty server.
+	 *
+	 * @param configuration Server configuration.
+	 */
+	public EmbeddedJetty(EmbeddedJettyConfiguration configuration) {
+		super(configuration);
+		log.warn("{} is deprecated and will be removed in the next major release, use junit-servers-tomcat-8 instead, see https://mjeanroy.dev/junit-servers", EmbeddedJetty.class);
+	}
+
+	@Override
+	protected String containerJarPatternPropertyName() {
+		return WebInfConfiguration.CONTAINER_JAR_PATTERN;
+	}
+
+	@Override
+	protected String webInfJarPatternPropertyName() {
+		return WebInfConfiguration.WEBINF_JAR_PATTERN;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		WebAppContext webAppContext = getWebAppContext();
+		return webAppContext == null ? null : webAppContext.getServletContext();
+	}
+}
