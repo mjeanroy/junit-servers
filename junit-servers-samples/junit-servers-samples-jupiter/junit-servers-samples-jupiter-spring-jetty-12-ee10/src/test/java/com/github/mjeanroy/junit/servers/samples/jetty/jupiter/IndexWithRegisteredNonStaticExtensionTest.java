24f70fcabc2f7d1b8a99c7315f48d381a850f358
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2026-02-14 21:11:15 +0100

    tests: add testing modules

---
+++ junit-servers-samples/junit-servers-samples-jupiter/junit-servers-samples-jupiter-spring-jetty-12-ee10/src/test/java/com/github/mjeanroy/junit/servers/samples/jetty/jupiter/IndexWithRegisteredNonStaticExtensionTest.java
@@ -0,0 +1,47 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2026 <mickael.jeanroy@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.github.mjeanroy.junit.servers.samples.jetty.jupiter;
+
+import com.github.mjeanroy.junit.servers.client.HttpClient;
+import com.github.mjeanroy.junit.servers.jetty12ee10.EmbeddedJetty;
+import com.github.mjeanroy.junit.servers.jetty12ee10.jupiter.JettyServerExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static com.github.mjeanroy.junit.servers.samples.utils.WebTestUtils.ensureIndexIsOk;
+import static com.github.mjeanroy.junit.servers.samples.utils.jetty.JettyTestUtils.createJettyConfiguration;
+
+class IndexWithRegisteredNonStaticExtensionTest {
+
+	@RegisterExtension
+	final JettyServerExtension jetty = new JettyServerExtension(
+		createJettyConfiguration()
+	);
+
+	@Test
+	void it_should_have_an_index(HttpClient client, EmbeddedJetty jetty) {
+		ensureIndexIsOk(client);
+	}
+}
